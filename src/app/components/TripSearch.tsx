2531fcce7c0a644a29883957f59a0a632ec9961f
uillasnr <wiliasreis@hotmail.com>
2023-07-11 19:18:57 -0300

    buscar

--- src/app/components/TripSearch.tsx
+++ src/app/components/TripSearch.tsx
@@ -7,7 +7,7 @@ import Input from "@/components/input";
 
 import { useRouter } from "next/navigation";
 import React from "react";
-//import { Controller, useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 
 interface TripSearchForm {
   text: string;
@@ -18,45 +18,44 @@ interface TripSearchForm {
 const TripSearch = () => {
   const router = useRouter();
 
-/*   const {
+  const {
     control,
     formState: { errors },
     register,
     handleSubmit,
-  } = useForm<TripSearchForm>(); */
+  } = useForm<TripSearchForm>();
 
   const onSubmit = (data: TripSearchForm) => {
     router.push(`/trips/search?text=${data.text}&startDate=${data.startDate?.toISOString()}&budget=${data.budget}`);
   };
 
   return (
-    <div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat">
-      <h1 className="font-semibold text-2xl text-primaryDarker text-center">
+    <div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat lg:py-28">
+      <h1 className="font-semibold text-2xl text-primaryDarker text-center lg:text-[2.5rem]">
         Encontre sua próxima <span className="text-primary">viagem!</span>
       </h1>
 
-      <div className="flex flex-col gap-4 mt-5">
-        <Input placeholder="Onde você quer ir?"
-        /*   error={!!errors.text}
+      <div className="flex flex-col gap-4 mt-5 lg:flex-row lg:max-w-[948px] lg:mx-auto lg:p-4 lg:bg-primary lg:mt-12 lg:bg-opacity-20 lg:rounded-lg">
+
+        <Input
+          placeholder="Onde você quer ir?"
+          error={!!errors.text}
           errorMessage={errors.text?.message}
           {...register("text", {
             required: {
               value: true,
               message: "Texto é obrigatório.",
             },
-          })} */
+          })}
         />
 
-        <div className="flex gap-4">
-        {/*    <Controller
-            name="startDate"
-            control={control}
-            render={({ field }) => ( 
-              <DatePicker placeholderText="Data Final" className="w-full" minDate={new Date()} />
-             )} 
-          /> */}
+        <div className="flex gap-4 lg:w-full">
+          <Controller name="startDate" control={control} render={({ field }) => (
+            <DatePicker onChange={field.onChange} selected={field.value} placeholderText="Data Final" className="w-full" minDate={new Date()} />
+          )}
+          />
 
-    {/*        <Controller
+          <Controller
             name="budget"
             control={control}
             render={({ field }) => (
@@ -67,11 +66,13 @@ const TripSearch = () => {
                 value={field.value}
                 onBlur={field.onBlur}
               />
-            )} 
-          /> */}
+            )}
+          />
         </div>
 
-        <Button >Buscar</Button>
+        <Button onClick={() => handleSubmit(onSubmit)()} className="w-1/2  w-full lg:h-fit">
+          Buscar
+        </Button>
       </div>
     </div>
   );
