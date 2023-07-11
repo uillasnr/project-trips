120f53a360bc676fc01fddc8a222ad0ed8ca4e43
uillasnr <wiliasreis@hotmail.com>
2023-07-11 10:01:39 -0300

    add validação da data de viagem

--- src/app/trips/[tripId]/components/TripReserVation.tsx
+++ src/app/trips/[tripId]/components/TripReserVation.tsx
@@ -1,59 +1,173 @@
-"use client"
+"use client";
+
 import Button from "@/components/Button";
 import DatePicker from "@/components/DatePicker";
 import Input from "@/components/input";
-import { Trip } from "@prisma/client";
+
+import { differenceInDays } from "date-fns";
+import { useRouter } from "next/navigation";
 import React from "react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 
 interface TripReservationProps {
-    trip: Trip
-
+    tripId: string;
+    tripStartDate: Date;
+    tripEndDate: Date;
+    maxGuests: number;
+    pricePerDay: number;
 }
+
 interface TripReservationForm {
-    guests: number
-
+    guests: number;
+    startDate: Date | null;
+    endDate: Date | null;
 }
 
-const TripReservation = (trip: TripReservationProps) => {
-    const { register, handleSubmit, formState: { errors },
-    } = useForm<TripReservationForm>()
+const TripReservation = ({ tripId, maxGuests, tripStartDate, tripEndDate, pricePerDay }: TripReservationProps) => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        control,
+        watch,
+        setError,
+    } = useForm<TripReservationForm>();
 
-    const onSubmit = (data: any) => {
-console.log({data})
-    }
+    const router = useRouter();
 
+    const onSubmit = async (data: TripReservationForm) => {
+        const response = await fetch("/api/trips/check", {
+            method: "POST",
+            body: Buffer.from(
+                JSON.stringify({
+                    startDate: data.startDate,
+                    endDate: data.endDate,
+                    tripId,
+                })
+            ),
+        });
+
+        const res = await response.json();
+
+        if (res?.error?.code === "TRIP_ALREADY_RESERVED") {
+            setError("startDate", {
+                type: "manual",
+                message: "Esta data já está reservada.",
+            });
+
+            return setError("endDate", {
+                type: "manual",
+                message: "Esta data já está reservada.",
+            });
+        }
+
+        if (res?.error?.code === "INVALID_START_DATE") {
+            return setError("startDate", {
+                type: "manual",
+                message: "Data inválida.",
+            });
+        }
+
+        if (res?.error?.code === "INVALID_END_DATE") {
+            return setError("endDate", {
+                type: "manual",
+                message: "Data inválida.",
+            });
+        }
+
+        router.push(
+            `/trips/${tripId}/confirmation?startDate=${data.startDate?.toISOString()}&endDate=${data.endDate?.toISOString()}&guests=${data.guests}`
+        );
+    };
+
+    const startDate = watch("startDate");
+    const endDate = watch("endDate");
 
     return (
-        <div>
-            <div className="flex flex-col px-5 ">
-                <div className="flex gap-4">
-                    <DatePicker placeholderText="Data de Inicio" onChange={() => { }} className="w-full" />
-                    <DatePicker placeholderText="Data Final" onChange={() => { }} className="w-full" />
-                </div>
+        <div className="flex flex-col px-5 lg:min-w-[380px] lg:p-5 lg:border-grayLighter lg:border lg:rounded-lg lg:shadow-md">
+            <p className="text-xl hidden text-primaryDarker mb-4 lg:block">
+                <span className="font-semibold">R${pricePerDay}</span> por dia
+            </p>
 
-                <Input placeholder={`Número de hóspedes (max: ${trip.maxGuests})`} className="mt-4"
-                    {...register("guests", {
+            <div className="flex gap-4">
+                <Controller
+                    name="startDate"
+                    rules={{
                         required: {
                             value: true,
-                            message: "Número de hóspedes é obrigatório."
+                            message: "Data inicial é obrigatória.",
                         },
-                    })}
-                    error={!!errors?.guests}
-                    errorMessage={errors?.guests?.message}
+                    }}
+                    control={control}
+                    render={({ field }) => (
+                        <DatePicker
+                            error={!!errors?.startDate}
+                            errorMessage={errors?.startDate?.message}
+                            onChange={field.onChange}
+                            selected={field.value}
+                            placeholderText="Data de Início"
+                            className="w-full"
+                            minDate={tripStartDate}
+                        />
+                    )}
                 />
 
-                <div className="flex justify-between mt-3">
-                    <p className="font-medium text-sm primaryDarker">Total</p>
-                    <p className="font-medium text-sm primaryDarker">R$ 2500</p>
-                </div>
+                <Controller
+                    name="endDate"
+                    rules={{
+                        required: {
+                            value: true,
+                            message: "Data final é obrigatória.",
+                        },
+                    }}
+                    control={control}
+                    render={({ field }) => (
+                        <DatePicker
+                            error={!!errors?.endDate}
+                            errorMessage={errors?.endDate?.message}
+                            onChange={field.onChange}
+                            selected={field.value}
+                            placeholderText="Data Final"
+                            className="w-full"
+                            maxDate={tripEndDate}
+                            minDate={startDate ?? tripStartDate}
+                        />
+                    )}
+                />
+            </div>
 
-                <div className="pb-10 border-b border-grayLighter w-full">
-                    <Button onClick={() => handleSubmit(onSubmit)()} className="mt-3 w-full">Reservar agora</Button>
-                </div>
+            <Input
+                {...register("guests", {
+                    required: {
+                        value: true,
+                        message: "Número de hóspedes é obrigatório.",
+                    },
+                    max: {
+                        value: maxGuests,
+                        message: `Número de hóspedes não pode ser maior que ${maxGuests}.`,
+                    },
+                })}
+                placeholder={`Número de hóspedes (max: ${maxGuests})`}
+                className="mt-4"
+                error={!!errors?.guests}
+                errorMessage={errors?.guests?.message}
+                type="number"
+            />
+
+            <div className="flex justify-between mt-3">
+                <p className="font-medium text-sm text-primaryDarker">Total: </p>
+                <p className="font-medium text-sm text-primaryDarker">
+                    {startDate && endDate ? `R$${differenceInDays(endDate, startDate) * pricePerDay}` ?? 1 : "R$0"}
+                </p>
+            </div>
+
+            <div className="pb-10 border-b border-b-grayLighter w-full lg:border-none lg:pb-0">
+                <Button onClick={() => handleSubmit(onSubmit)()} className="mt-3 w-full">
+                    Reservar agora
+                </Button>
             </div>
         </div>
     );
 };
 
-export default TripReservation
+export default TripReservation;
--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -1,20 +1,22 @@
 //Pagina de Detalhes da trip
 import { prisma } from "@/lib/prisma";
+
 import React from "react";
 import TripHeader from "./components/TripHeader";
-import TripReservation from "./components/TripReserVation";
 import TripDescription from "./components/TripDescription";
 import TripHighlights from "./components/TripHighlights";
 import TripLocation from "./components/TripLocation";
+import TripReservation from "./components/TripReserVation";
 
 const getTripDetails = async (tripId: string) => {
     const trip = await prisma.trip.findUnique({
         where: {
             id: tripId,
         },
-    })
-    return trip
-}
+    });
+
+    return trip;
+};
 
 const TripDetails = async ({ params }: { params: { tripId: string } }) => {
     const trip = await getTripDetails(params.tripId);
@@ -22,14 +24,28 @@ const TripDetails = async ({ params }: { params: { tripId: string } }) => {
     if (!trip) return null;
 
     return (
-        <div className="container mx-auto">
+        <div className="container mx-auto lg:px-40 lg:pt-10">
             <TripHeader trip={trip} />
-            <TripReservation trip={trip} />
-            <TripDescription description={trip.description} />
-            <TripHighlights highlights={trip.highlights} />
+            <div className="flex flex-col lg:flex-row lg:mt-12 lg:gap-20">
+                <div className="lg:order-2">
+                    <TripReservation
+                        tripId={trip.id}
+                        pricePerDay={trip.pricePerDay as any}
+                        tripStartDate={trip.startDate}
+                        tripEndDate={trip.endDate}
+                        maxGuests={trip.maxGuests}
+                    />
+                </div>
+
+                <div className="lg:order-1">
+                    <TripDescription description={trip.description} />
+                    <TripHighlights highlights={trip.highlights} />
+                </div>
+            </div>
+            {/*       <TripLocation locationDescription={trip.locationDescription} location={trip.location} /> */}
             <TripLocation location={trip.location} />
         </div>
     );
 };
 
-export default TripDetails
+export default TripDetails;
